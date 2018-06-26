b053209d259907c7d4bc14bebdca897924955a3c
saaav <draculav@hotmail.com>
2018-06-26 18:41:36 +0800

    start

--- src/main/java/com/saaavsaaa/client/zookeeper/core/BaseClientFactory.java
+++ src/main/java/com/saaavsaaa/client/zookeeper/core/BaseClientFactory.java
@@ -2,10 +2,13 @@ package com.saaavsaaa.client.zookeeper.core;
 
 import com.saaavsaaa.client.action.IClient;
 import com.saaavsaaa.client.zookeeper.section.Listener;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.data.ACL;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE;
 
@@ -29,15 +32,38 @@ public abstract class BaseClientFactory {
      * @throws InterruptedException InterruptedException
      */
     public IClient start() throws IOException, InterruptedException {
+        prepareClient();
+        client.start();
+        return client;
+    }
+    
+    /**
+     * start until Timeout.
+     *
+     * @param wait wait
+     * @param units units
+     * @return connected
+     * @throws IOException IO Exception
+     * @throws InterruptedException InterruptedException
+     * @throws KeeperException OperationTimeoutException
+     */
+    public IClient start(final int wait, final TimeUnit units) throws IOException, InterruptedException, KeeperException {
+        prepareClient();
+        if (!client.start(wait, units)) {
+            client.close();
+            throw new KeeperException.OperationTimeoutException();
+        }
+        return client;
+    }
+    
+    private void prepareClient() {
         client.setRootNode(namespace);
         if (scheme == null) {
-            authorities = OPEN_ACL_UNSAFE;
+            authorities = ZooDefs.Ids.OPEN_ACL_UNSAFE;
         }
-        client.setAuthorities(scheme , auth, authorities);
-        client.start();
+        client.setAuthorities(scheme, auth, authorities);
         if (globalListener != null) {
             client.registerWatch(globalListener);
         }
-        return client;
     }
 }
